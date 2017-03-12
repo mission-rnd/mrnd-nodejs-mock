48eba5466de8aae7693bca3298ceced3ed576cb9
Abhijith Ravuri <souravabhijith@gmail.com>
2017-03-12 09:14:41 +0530

    First commit

---
+++ src/models/tickets.js
@@ -0,0 +1,10 @@
+/**
+ * Fill this method to create required tables for storing tickets.
+ *
+ * Also, this method should clear off any existing data.
+ *
+ * @param callback Send the err and result of the DDL operation appropriately
+ */
+exports.setupTicketsTable = function (callback) {
+
+};
---
+++ src/routes/tickets.js
@@ -0,0 +1,5 @@
+/**
+ * Route all the requests to /tickets to this file....
+ *
+ * Use models/tickets.js to perform appropriate CRUD Operations.
+ */
